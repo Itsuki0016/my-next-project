6ccdce577f9b3fa83fa7ece8055d4e5ac80c08f0
Itsuki0016 <167990332+Itsuki0016@users.noreply.github.com>
2024-11-09 15:04:33 +0900

    11/9 first commit
    page.82

---
+++ app/_components/Footer/index.tsx
@@ -0,0 +1,28 @@
+import style from './index.module.css';
+
+export default function Footer() {
+    return (
+        <footer className={style.footer}>
+                <nav className={style.nav}>
+                    <ul className={style.items}>
+                        <li className={style.item}>
+                            <a href="/news" >
+                                ニュース
+                            </a>
+                        </li>
+                        <li className={style.item}>
+                            <a href="/members">
+                                メンバー
+                            </a>
+                        </li>
+                        <li className={style.item}>
+                            <a href="/contact">
+                                お問い合わせ
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            <p className={style.cr}>©︎ SIMPLE. ALL Rights Reserved 2024</p>
+        </footer>
+    );
+}
--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 
 import styles from './index.module.css';
+import Category from '../Category';
+import Date from '../Date';
+import { News } from '@/app/libs/microcms';
 
 type Props = {
     news: News[];
@@ -11,37 +14,27 @@ export default function NewsList({ news }: Props) {
         return <p>記事がありません。</p>;
         }
         return (
-        <ul>
-            {news.map((article) => (
-            <li key={article.id} className={styles.list}>
-                <Link href={`/news/${article.id}`} className={styles.link}>
-                {article.thumbnail ? (
-                    <Image
-                    src={article.thumbnail.url}
-                    alt=""
-                    className={styles.image}
-                    width={article.thumbnail.width}
-                    height={article.thumbnail.height}
-                    />
-                ) : (
-                    <Image
-                    className={styles.image}
-                    src="/no-image.png"
-                    alt="No Image"
-                    width={1200}
-                    height={630}
-                    />
-                )}
-                <dl className={styles.content}>
-                    <dt className={styles.title}>{article.title}</dt>
-                    <dd className={styles.meta}>
-                    <Category category={article.category} />
-                    <Date date={article.publishedAt ?? article.createdAt} />
-                    </dd>
-                </dl>
-                </Link>
-            </li>
+            <ul>
+                {news.map((article) => (
+                    <li key={article.id} className={styles.list}>
+                        <div className={styles.link}> 
+                            <Image
+                            className={styles.image}
+                            src="/no-image.png"
+                            alt="No Image"
+                            width={1200}
+                            height={630}
+                            />
+                            <dl className={styles.content}>
+                                <dt className={styles.title}>{article.title}</dt>
+                                <dd className={styles.meta}>
+                                    <Category category={article.category} />
+                                    <Date date={article.publishedAt  ?? article.createdAt}/>
+                            </dd>
+                        </dl>
+                    </div>
+                </li>
             ))}
         </ul>
-        );
-    }
+    );
+}
