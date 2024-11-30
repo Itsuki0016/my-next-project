7c0015bcbd280070de65cb65bf472f1acf8da6a5
Itsuki0016 <167990332+Itsuki0016@users.noreply.github.com>
2024-11-30 15:36:07 +0900

    11/30 second commit
    page 212

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation';
 import { getNewsDetail } from '@/app/libs/microcms';
-import Article from "@/app/_components/Article";
+import Article from '@/app/_components/Article';
 import ButtonLink from '@/app/_components/ButtonLink';
 import styles from './page.module.css';
 
@@ -8,10 +8,15 @@ type Props = {
     params: {
         slug: string;
     };
+    searchParams: {
+        dk?: string;
+    };
 };
 
-    export default async function Page({ params }: Props) {
-    const data = await getNewsDetail(params.slug).catch(notFound);
+export default async function Page({ params, searchParams }: Props) {
+    const data = await getNewsDetail(params.slug, {
+        draftKey: searchParams.dk,
+    }).catch(notFound);
 
     return (
         <>
